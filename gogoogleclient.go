858c07690bfb3ebe9e60a9679d08037695197327
Stephen Cornelius <scornelius@gmail.com>
2017-02-20 17:47:35 +0000

    SC - vendored dependencies

--- gogoogleclient.go
+++ gogoogleclient.go
@@ -4,30 +4,30 @@ Package gogoogleclient helps service apps connect to google apis
 package gogoogleclient
 
 import (
-  "os"
-  "log"
-  "io/ioutil"
-  "net/http"
-  "golang.org/x/oauth2"
-  "golang.org/x/oauth2/google"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 // Client returns a google service client
 func Client(scope string) *http.Client {
-  configVariables := loadApplicationCredentials()
-  jwtConfig, error := google.JWTConfigFromJSON(configVariables, scope)
-  if error != nil {
-    log.Fatal(error)
-  }
-  client := jwtConfig.Client(oauth2.NoContext)
-  return client
+	configVariables := loadApplicationCredentials()
+	jwtConfig, error := google.JWTConfigFromJSON(configVariables, scope)
+	if error != nil {
+		log.Fatal(error)
+	}
+	client := jwtConfig.Client(oauth2.NoContext)
+	return client
 }
 
 // loadConfigurationVariables gets the service account info from environment variables and returns them as json
 func loadApplicationCredentials() []byte {
-  credentialFile,error := ioutil.ReadFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-  if error != nil {
-    log.Fatal(error)
-  }
-  return credentialFile
+	credentialFile, error := ioutil.ReadFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+	if error != nil {
+		log.Fatal(error)
+	}
+	return credentialFile
 }
--- gogoogleclient_test.go
+++ gogoogleclient_test.go
@@ -1,19 +1,18 @@
 package gogoogleclient
 
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
-  _ "net/http"
+	"github.com/stretchr/testify/assert"
+	_ "net/http"
+	"testing"
 )
 
 func TestClient(test *testing.T) {
-  client := Client("https://www.googleapis.com/auth/analytics")
-  //assert.IsType(test, client, *http.Client, "It should return an http client for a valid scope")
-  assert.NotNil(test, client, "It should return an http client for a valid scope")
+	client := Client("https://www.googleapis.com/auth/analytics")
+	//assert.IsType(test, client, *http.Client, "It should return an http client for a valid scope")
+	assert.NotNil(test, client, "It should return an http client for a valid scope")
 }
 
 func TestLoadApplicationCredentials(test *testing.T) {
-  jsonconfig := loadApplicationCredentials()
-  assert.NotNil(test, jsonconfig, "It should extract the relevant environment variables as JSON")
+	jsonconfig := loadApplicationCredentials()
+	assert.NotNil(test, jsonconfig, "It should extract the relevant environment variables as JSON")
 }
-
